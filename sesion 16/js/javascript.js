e5b81482fd5df87b1306a1e34d5ab35a348a5d5f
sam <samanthamendoza821@gmail.com>
2021-05-23 18:49:54 -0500

    sesion 16

---
+++ sesion 16/js/javascript.js
@@ -0,0 +1,17 @@
+function compara(){
+    var a=2;
+    var b=5;
+
+    if(a==b) alert("a y b son iguales")
+    else alert("a y b son distintos")
+}
+
+function resta(x,y){
+ var resta=x-y;
+ document.write("<h4> La resta es "+resta+"</h4>");
+}
+
+function suma(a,b){
+    var suma =a+b;
+    document.getElementById("sumar").innerHTML="la suma es "+suma;
+}
